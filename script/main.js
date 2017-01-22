ad0954e147fc8bcf4f2ff525becff5f369111de5
rorysaxby <rory.saxby@gmail.com>
2017-01-22 21:27:01 +0000

    new javascript carousel proto

--- script/main.js
+++ script/main.js
@@ -1,7 +1,18 @@
 (function(){
 	'use strict';
 
-	
+	var exampleCarousel = new rslib.carousel({
+		containerId: 'rsCarousel',
+		viewTarget: 'li',
+		activeClass: 'active',
+		nextClass: 'right',
+		prevClass: 'left',
+		startAt: 1,
+		autoRotate: true,
+		rotateTime: 3000
+	});
+
+
 	/*var profNotif = new rtCarousel();
 
 	profNotif.opts = {
@@ -26,46 +37,46 @@
 		navControl: true
 	});*/
 
-	var calc = {};
+	// var calc = {};
 
-	calc.answer = 3;
+	// calc.answer = 3;
 
-	calc.runCount = 0;
-	calc.num1 = 0;
-	calc.num2 = 1;
-	calc.num3 = 2;
-	calc.decimal = 20;
+	// calc.runCount = 0;
+	// calc.num1 = 0;
+	// calc.num2 = 1;
+	// calc.num3 = 2;
+	// calc.decimal = 20;
 
-	calc.setCalc = function(){
-		calc.runCount++;
-		calc.num1 = calc.num1 + 2;
-		calc.num2 = calc.num2 + 2;
-		calc.num3 = calc.num3 + 2;
+	// calc.setCalc = function(){
+	// 	calc.runCount++;
+	// 	calc.num1 = calc.num1 + 2;
+	// 	calc.num2 = calc.num2 + 2;
+	// 	calc.num3 = calc.num3 + 2;
 
-		if(calc.runCount % 2 === 0){
-			calc.runNegCalc();
-		} else{
-			calc.runPosCalc();
-		};
-	};
+	// 	if(calc.runCount % 2 === 0){
+	// 		calc.runNegCalc();
+	// 	} else{
+	// 		calc.runPosCalc();
+	// 	};
+	// };
 
-	calc.runPosCalc = function(){
-		var pi = 4 / (calc.num1 * calc.num2 * calc.num3);
-		calc.answer = calc.answer + pi;
-		console.log(calc.answer.toFixed(calc.decimal));
-	};
+	// calc.runPosCalc = function(){
+	// 	var pi = 4 / (calc.num1 * calc.num2 * calc.num3);
+	// 	calc.answer = calc.answer + pi;
+	// 	console.log(calc.answer.toFixed(calc.decimal));
+	// };
 
-	calc.runNegCalc = function(){
-		var pi = 4 / (calc.num1 * calc.num2 * calc.num3);
-		calc.answer = calc.answer - pi;
-		console.log(calc.answer.toFixed(calc.decimal));
-	};
+	// calc.runNegCalc = function(){
+	// 	var pi = 4 / (calc.num1 * calc.num2 * calc.num3);
+	// 	calc.answer = calc.answer - pi;
+	// 	console.log(calc.answer.toFixed(calc.decimal));
+	// };
 
-	calc.init = function(){
-		setInterval(function(){
-			calc.setCalc();
-		},1000);
-	};
+	// calc.init = function(){
+	// 	setInterval(function(){
+	// 		calc.setCalc();
+	// 	},1000);
+	// };
 
 	//calc.init();
 
--- script/rslib/rslib.carousel.js
+++ script/rslib/rslib.carousel.js
@@ -38,22 +38,31 @@
 (function(){
 	
 	rslib.carousel = function(opts){
+
 		this.opts = {};
-		this.opts.viewsContainer = $(opts.viewsContainer);
-		this.opts.views = this.opts.viewsContainer.find('> div');
-		this.opts.tabsContainer = $(opts.tabsContainer);
-		this.opts.activeTabClass = opts.activeTabClass;
-		this.opts.hiddenViewClass = opts.hiddenViewClass;
+
+		// user defined
+		this.opts.container = $("#" + opts.containerId);
+		this.opts.viewTarget = opts.viewTarget;
+		this.opts.activeClass = opts.activeClass;
+		this.opts.nextClass = opts.nextClass;
+		this.opts.prevClass = opts.prevClass;
 		this.opts.startAt = (opts.startAt - 1) || 0;
+		this.opts.autoRotate = opts.autoRotate || false;
+		this.opts.rotateTime = opts.rotateTime || 5000;
+
+		this.opts.currentIndex = this.opts.startAt;
+		this.opts.viewList = this.opts.container.find(this.opts.viewTarget);
+		
 		// initialise carousel
 		this.init();
 	};
 
 	rslib.carousel.prototype.checkOpts = function(){
-		if(this.opts.viewsContainer == undefined){
+		if(this.opts.container == undefined){
 			console.log('Please specify option: viewContainer');
 			return false;
-		} else if(this.opts.tabsContainer == undefined){
+		} else if(this.opts.viewTarget == undefined){
 			console.log('Please specify option: tabsContainer');
 			return false;
 		} else{
@@ -61,10 +70,46 @@
 		};
 	};
 
+	rslib.carousel.prototype.resetViews = function(){
+		for(var i = 0; i < this.opts.viewList.length; i++){
+			this.opts.viewList[i].className = "";
+		};
+	};
+
+	rslib.carousel.prototype.setViewActive = function(index){
+		$(this.opts.viewList[index]).addClass(this.opts.activeClass);
+	};
+
+	rslib.carousel.prototype.setNext = function(index){
+		var next = index + 1;
+		if(next > this.opts.viewList.length - 1){
+			next = 0;
+		};
+		$(this.opts.viewList[next]).addClass(this.opts.activeClass + " " + this.opts.nextClass);
+	};
+
+	rslib.carousel.prototype.setPrev = function(index){
+		var prev = index - 1;
+		if(prev < 0){
+			prev = this.opts.viewList.length - 1;
+		};
+		$(this.opts.viewList[prev]).addClass(this.opts.activeClass + " " + this.opts.prevClass);
+	};
+
+	rslib.carousel.setAutoRotate = function(){
+		setInterval(this.selectNext(), this.opts.rotateTime);
+	};
+
+	rslib.carousel.setNext = function(){
+
+	};
 
 	rslib.carousel.prototype.init = function(){
 		if(this.checkOpts()){
-
+			this.resetViews();
+			this.setViewActive(this.opts.startAt);
+			this.setNext(this.opts.startAt);
+			this.setPrev(this.opts.startAt);
 		};
 	};
 
