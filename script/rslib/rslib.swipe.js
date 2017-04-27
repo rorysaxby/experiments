17689cd4cb93f4b32fcca1db9ccb6675dbf3ffa9
Rory Saxby <rory@rocktime.local>
2017-04-27 10:58:51 +0100

    swipe detection

--- script/rslib/rslib.swipe.js
+++ script/rslib/rslib.swipe.js
@@ -11,31 +11,74 @@ rslib.swipe = function(opts){
 	this.opts = {};
 
 	this.opts.target = $(opts.target);
-	this.opts.animStyle = opts.style;
+	this.opts.yPixelRestrict = opts.yPixelRestrict || 50;
 	this.opts.leftFn = opts.leftFn;
 	this.opts.rightFn = opts.rightFn;
-
 	this.bindTargetTouches();
 };
 
 rslib.swipe.prototype.bindTargetTouches = function(){
 
+	var _ = this;
+
+	_.directionCheck = false,
+	_.xActive = false;
+
 	this.opts.target[0].addEventListener('touchstart', function(event) {
-		this.touchStartX = event.changedTouches[0].screenX;
-		this.touchStartY = event.changedTouches[0].screenY;
+		_.touchStartX = event.changedTouches[0].screenX;
+		_.touchStartY = event.changedTouches[0].screenY;
 	}, false);
 
 	this.opts.target[0].addEventListener('touchmove', function(event) {
-	    this.touchPosX = event.changedTouches[0].screenX;
-	    this.touchPosY = event.changedTouches[0].screenY;
+	    _.touchPosX = event.changedTouches[0].screenX;
+	    _.touchPosY = event.changedTouches[0].screenY;
+	    _.xDist = _.touchStartX - _.touchPosX,
+		_.yDist = _.touchStartY - _.touchPosY;
+
+	    _.touchDirection();
+	    
 	}, false);
 
 	this.opts.target[0].addEventListener('touchend', function(event) {
-	    this.touchEndX = event.changedTouches[0].screenX;
-	    this.touchEndY = event.changedTouches[0].screenY;
+	    _.touchEndX = event.changedTouches[0].screenX;
+	    _.touchEndY = event.changedTouches[0].screenY;
+	    _.directionCheck = false,
+		_.xActive = false;
 	}, false);
 };
 
-rslib.swipe.prototype.compareTouchEvents = function(){
-	
+rslib.swipe.prototype.touchDirection = function(){
+	if (!this.directionCheck) {
+    	this.directionCheck = true;
+    	this.compareDist();
+    };
+
+    if(this.xActive && this.directionCheck) {
+    	console.log(this.swipeDirection + ": " + this.xDist);
+    };
+};
+
+rslib.swipe.prototype.compareDist = function(){
+	var x = this.xDist,
+		y = this.yDist;
+		
+	if(x < 0){
+		x = Math.abs(x);
+	};
+
+	if(y < 0){
+		y = Math.abs(y);
+	};
+
+	if (this.xDist > 0){
+		if(x > y){
+			this.xActive = true;
+			this.swipeDirection = 'left';
+		};
+	} else{
+		if(x > y){
+			this.xActive = true;
+			this.swipeDirection = 'right';
+		};
+	};
 };
